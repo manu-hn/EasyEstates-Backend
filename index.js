26bc3c913fa51ca57ab93b16621a02f4aba4f6f2
manu-hn <manuu.hn11@gmail.com>
2024-04-14 17:23:15 +0530

    CORS Origin Updated

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
 }))
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with your frontend URL
+    next();
+});
 
 app.use('/api/easy-estates', UserRoutes);
 app.use('/api/properties', ListingsRoutes);
